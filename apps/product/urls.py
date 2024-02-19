1e2ac2c325c3f86807a4f15cde132b1b650e41fb
Alisher <Alishka@gmail.com>
2024-02-19 18:27:42 +0600

    startapp product18

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -13,6 +13,7 @@ from .views import(
     ProductFilterListAPIView,
     CartAPIView,
     CartUpdateAPIView,
+    CartResetAPIView
 )
 
 urlpatterns = [
@@ -27,6 +28,5 @@ urlpatterns = [
     path('filter/', ProductFilterListAPIView.as_view(), name='product-filter'),
     path('cart/<int:id>', CartAPIView.as_view(), name='cart'),
     path('cart/<int:id>/update/', CartUpdateAPIView.as_view(), name='cart-update'),
-
-
+    path('cart/<int:id>/reset/', CartResetAPIView.as_view(), name='cart-reset'),
 ]
