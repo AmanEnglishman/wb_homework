c64015632de0755e8933d5528424ce1a1988f990
Alisher <Alishka@gmail.com>
2024-02-19 18:18:39 +0600

    startapp product15

--- apps/product/views.py
+++ apps/product/views.py
@@ -28,3 +28,23 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
     permission_classes = [permissions.AllowAny]
     queryset = Product.objects.all()
+
+
+class CategoryListAPIView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryCreateAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        data = request.data.get('data', [])
+        for category_data in data:
+            serializer = CategorySerializer(data=category_data)
+            if serializer.is_valid():
+                name = category_data['name']
+                Category.objects.get_or_create(name=name)
+        return Response({'response': 'created'}, status=status.HTTP_201_CREATED)
+
