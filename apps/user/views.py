c67037403e8f28e3e8a59dfe62afd8ccfe38f177
Your Name <you@example.com>
2024-02-19 17:05:32 +0600

    add register view

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,11 @@
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from apps.user.models import User
 from apps.user.permissions import AnonPermission
-from apps.user.serializers import MyTokenSerializer
+from apps.user.serializers import MyTokenSerializer, UserSerializer
 
 
 class LoginAPIView(TokenObtainPairView):
@@ -9,3 +13,21 @@ class LoginAPIView(TokenObtainPairView):
     serializer_class = MyTokenSerializer
 
 
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            seller = User.objects.create(
+                email=request.data['email'],
+                name=request.data['name'],
+                second_name=request.data['second_name'],
+                phone=request.data['phone'],
+                address=request.data['address'],
+                is_Seller=True,
+            )
+            seller.set_password(request.data['password'])
+            seller.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
