e84c0abb8d45f0cc3f87fef9d24a19d352c6fd66
Your Name <you@example.com>
2024-02-19 20:53:14 +0600

    custom admin products

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Cart, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name'
+    )
+    list_display_links = (
+        'name',
+        'id'
+    )
+    list_filter = (
+        'name',
+        'id'
+    )
+    search_fields = (
+        'name',
+        'id'
+    )
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name'
+    )
+    list_display_links = (
+        'name',
+        'id'
+    )
+    list_filter = (
+        'name',
+        'id'
+    )
+    search_fields = (
+        'name',
+        'id'
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name'
+    )
+    list_display_links = (
+        'name',
+        'id'
+    )
+    list_filter = (
+        'name',
+        'id'
+    )
+    search_fields = (
+        'name',
+        'id'
+    )
